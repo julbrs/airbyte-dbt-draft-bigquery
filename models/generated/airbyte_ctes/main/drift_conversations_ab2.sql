752885e3a37029c287e0f52d31698c4334bfdc22
Julien Bras <julienbras@sidoine.org>
2021-10-30 09:49:37 -0400

    incremental adjustement

--- models/generated/airbyte_ctes/main/drift_conversations_ab2.sql
+++ models/generated/airbyte_ctes/main/drift_conversations_ab2.sql
@@ -1,7 +1,7 @@
 {{ config(schema="_airbyte_main", tags=["top-level-intermediate"]) }}
 -- SQL model to cast each column to its adequate SQL type converted from the JSON schema type
 select
-    cast(id as {{ dbt_utils.type_bigint() }}) as id,
+    cast(id as {{ dbt_utils.type_string() }}) as id,
     cast(status as {{ dbt_utils.type_string() }}) as status,
     cast(inboxId as {{ dbt_utils.type_bigint() }}) as inboxId,
     cast(contactId as {{ dbt_utils.type_bigint() }}) as contactId,
--- models/generated/airbyte_tables/main/tickets.sql
+++ models/generated/airbyte_tables/main/tickets.sql
@@ -1,4 +1,10 @@
-{{ config(schema="main", tags=["top-level"]) }}
+{{ config(
+    schema="main", 
+    tags=["top-level"], 
+    materialized='incremental', 
+    unique_key='_airbyte_hashid', 
+    on_schema_change='append_new_columns',
+    ) }}
 -- Final base SQL model
 select
     'drift' as source,
@@ -7,7 +13,7 @@ select
     TIMESTAMP_MILLIS(createdAt) as created_at,
     TIMESTAMP_MILLIS(updatedAt) as updated_at,
     _airbyte_emitted_at,
-    _airbyte_drift_conversations_hashid
+    _airbyte_drift_conversations_hashid as _airbyte_hashid
 from {{ ref('drift_conversations_ab3') }}
 -- drift_conversations from {{ source('main', '_airbyte_raw_drift_conversations') }}
 
